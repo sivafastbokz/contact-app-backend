15ce854dd4912f2e930ecc2f8f828877b3d653a8
sivafastbokz <sivaharshanfastbokz@gmail.com>
2023-06-06 13:36:40 +0530

    get and postupdated

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 const Users =require('./dataschema');
+const port = 5000;
 
 app.use(express.json());
 app.use(cors());
@@ -13,9 +14,11 @@ mongoose.connect("mongodb+srv://sivaharshanfastbokz:uoazQaGUCRMUERcC@cluster0.lc
 }
 );
 
-app.get('/',async(req,res)=>{
-        const usename = new Users({name:"siva"})
-
+app.post('/adduser',async(req,res)=>{
+        const userName = req.body.userName
+        const usename = new Users({
+          userName:userName
+        })
         try{
           await usename.save();
           res.send("inserted data")
@@ -24,7 +27,16 @@ app.get('/',async(req,res)=>{
         }
 });
 
-const port = 7000;
+app.get('/getuserlist',async(req,res)=>{
+  try{
+    const data = await Users.find();
+    res.json(data)
+  }
+  catch(error){
+    res.status(500).json({message: error.message})
+  }
+});
+
 app.listen(port,()=>{
-    console.log('server is started on port 7000')
+    console.log('server is started on port 5000')
 })
